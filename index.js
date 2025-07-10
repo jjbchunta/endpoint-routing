23756ad47923a7226ce9ed9f178ea917a08ae90c
Joseph Chunta <jjbchunta@gmail.com>
2025-07-10 17:39:33 -0400

    .js endpoint file validation happens completely outside of the main compiling script >:)

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 import initializeRouting from './lib/making-requests.js';
 import buildEndpointRoutes from './lib/building-routes.js';
 import endpointRoutingPackageManager from './lib/package-manager.js';
-import { ENDPOINT_TYPES_GROUP } from './lib/constants.js';
-
-endpointRoutingPackageManager.push(
-    ENDPOINT_TYPES_GROUP,
-    'js',
-    {}
-);
 
 export default initializeRouting;
 export { buildEndpointRoutes, endpointRoutingPackageManager };
