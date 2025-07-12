87e9cafda2091feee32690401cf602adf57d0d96
Joseph Chunta <jjbchunta@gmail.com>
2025-07-12 08:01:15 -0400

    Full support for the new endpoint file standard is in place.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import initializeRouting from './lib/making-requests.js';
 import buildEndpointRoutes from './lib/building-routes.js';
 import endpointRoutingPackageManager from './lib/package-manager.js';
 import './lib/internal-packages.js';
+import './lib/external-packages.cjs';
 
 export default initializeRouting;
 export { buildEndpointRoutes, endpointRoutingPackageManager };
